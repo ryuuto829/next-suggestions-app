760d2b071eb49891969aaed66c8fc85770b1f8c3
ryuuto829 <ryuuto829@gmail.com>
2022-05-12 13:28:17 +0300

    chore: add fonts, icons, UI components lib

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', '-apple-system'],
+      serif: ['Inter', '-apple-system'],
+    },
+    fontWeight: {
+      regular: '400',
+      medium: '600',
+      bold: '800',
+    },
     extend: {},
   },
   plugins: [],
