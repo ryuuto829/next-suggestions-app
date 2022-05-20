98ed0fcbe8405906066cb47aa9c9ce0c5c0d4bf6
ryuuto829 <ryuuto829@gmail.com>
2022-05-20 20:51:33 +0300

    refactor: update select menu

--- components/SortingSelect.tsx
+++ components/SortingSelect.tsx
@@ -1,35 +1,35 @@
 import { Listbox } from '@headlessui/react'
-import { CodeIcon } from '@primer/octicons-react'
-
-import SortingOption from '@components/SortingOption'
+import { CodeIcon, ClockIcon, ZapIcon } from '@primer/octicons-react'
 
 const selectOptions = [
-  { icon: 'clock', name: 'Recent' },
-  { icon: 'zap', name: 'Most voted' },
+  { icon: <ClockIcon size={16} className="h-4 w-4" aria-hidden="true" />, name: 'Recent' },
+  { icon: <ZapIcon size={16} className="h-4 w-4" aria-hidden="true" />, name: 'Most voted' },
 ]
 
 export default function SortingSelect() {
-  const selected = selectOptions[1] // TODO: change to useState()
+  const selected = selectOptions[1]
 
   return (
     <div className="relative w-44 text-sm text-[#B2B8CD]">
-      <Listbox value={selected} onChange={() => console.log('select')}>
+      <Listbox value={selected} onChange={() => console.log('[SortingSelect] select')}>
         <Listbox.Button className="flex justify-between items-center w-full py-1.5 px-3 rounded bg-[#242837] hover:bg-[#2b3040]">
           <span className="flex items-center">
-            <SortingOption icon={selected.icon} name={selected.name} />
+            <span className="mr-2">{selected.icon}</span>
+            <span className="block truncate">{selected.name}</span>
           </span>
           <span className="flex items-center">
             <CodeIcon size={16} className="rotate-90 h-4 w-4" aria-hidden="true" />
           </span>
         </Listbox.Button>
         <Listbox.Options className="absolute mt-1 py-1 bg-[#242837] w-full rounded overflow-hidden">
-          {selectOptions.map((selected, key) => (
+          {selectOptions.map((option, key) => (
             <Listbox.Option
               key={key}
-              value={selected}
+              value={option}
               className="flex items-center w-full hover:bg-[#2b3040] py-1.5 px-3 cursor-pointer"
             >
-              <SortingOption icon={selected.icon} name={selected.name} />
+              <span className="mr-2">{option.icon}</span>
+              <span className="block truncate">{option.name}</span>
             </Listbox.Option>
           ))}
         </Listbox.Options>
