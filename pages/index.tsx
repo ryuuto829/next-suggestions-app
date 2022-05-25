d7557ca8c96be28909fa13e3b536e2a8f239bf84
ryuuto829 <ryuuto829@gmail.com>
2022-05-25 21:19:39 +0300

    feat: add simple suggestions form

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import SortingSelect from '@components/SortingSelect'
 import FilterMenu from '@components/FilterMenu'
 import NavigationBar from '@components/NavigationBar'
 import SigninModal from '@components/SigninModal'
+import SuggestionModal from '@components/SuggestionModal'
 import { useAuth } from '@lib/auth'
 
 const posts = [
@@ -33,6 +34,7 @@ const posts = [
 export default function Home() {
   const router = useRouter()
   const isLogin = router.asPath === '/login'
+  const isNewPost = router.asPath === '/new-post'
 
   const { user, signInWithGoogle, signOut } = useAuth()
 
@@ -53,18 +55,24 @@ export default function Home() {
     router.push('/', '/', { shallow: true })
   }
 
+  const handleNewPost = () => {
+    router.push('/', '/new-post', { shallow: true })
+  }
+
   return (
     <>
       <Head>
         <title>{isLogin ? 'Sign in | Next Suggestions App' : 'Next Suggestions App'}</title>
       </Head>
 
-      <NavigationBar user={user} handleSignOut={handleSignOut} handleSignIn={handleSignIn} />
       <SigninModal
         isLogin={isLogin}
         handleModalClose={handleModalClose}
         handleSignInWithGoogle={handleSignInWithGoogle}
       />
+      <SuggestionModal isOpen={isNewPost} handleModalClose={handleModalClose} />
+
+      <NavigationBar user={user} handleSignOut={handleSignOut} handleSignIn={handleSignIn} />
 
       <div className="pt-0 sm:pt-32">
         <main className="bg-[color:var(--dark-blue-charcoal-color)] max-w-2xl mx-auto rounded sm:min-h-full min-h-screen">
@@ -82,12 +90,15 @@ export default function Home() {
               <FilterMenu />
             </div>
             <div className="fixed bottom-0 left-0 w-full p-5 sm:p-0 sm:static sm:w-auto">
-              <button className="bg-[color:var(--purple-color)] py-2 px-4 rounded text-sm w-full hover:bg-[#453fc0]">
+              <button
+                onClick={handleNewPost}
+                className="bg-[color:var(--purple-color)] py-2 px-4 rounded text-sm w-full hover:bg-[#453fc0]"
+              >
                 Make suggestion
               </button>
             </div>
           </div>
-          <div className="px-5 sm:px-16 sm:py-12 py-8">
+          <div className="px-5 sm:px-16 sm:py-8 py-6">
             {posts &&
               posts.map((post) => (
                 <div
@@ -98,7 +109,7 @@ export default function Home() {
                     <div className="cursor-pointer w-full">
                       <div className="flex items-center">
                         <h2 className="font-medium">{post.title}</h2>
-                        <span className="mx-1 text-[color:var(--dark-gray-charcoal-color)]]">{`#${post.id}`}</span>
+                        <span className="mx-2 text-[color:var(--dark-gray-charcoal-color)]">{`#${post.id}`}</span>
                       </div>
                       <span className="text-sm text-gray-200 mt-2 block">
                         {post.comments[0].content}
@@ -107,14 +118,14 @@ export default function Home() {
                         <span className="text-xs px-2 py-1 bg-[color:var(--blue-charcoal-color)] hover:bg-[color:var(--light-blue-charcoal-color)] rounded mr-4">
                           {post.topic}
                         </span>
-                        <span className="flex items-center text-gray-200 text-sm">
+                        <span className="flex items-center text-[color:var(--dark-gray-charcoal-color)] text-sm">
                           <CommentIcon size={16} className="mr-1" />
                           <span>{post.comments.length}</span>
                         </span>
                       </div>
                     </div>
                   </Link>
-                  <button className="flex items-center text-gray-200 bg-[color:var(--blue-charcoal-color)] hover:bg-[color:var(--light-blue-charcoal-color)] rounded py-2 px-4">
+                  <button className="flex items-center text-gray-200 bg-[color:var(--blue-charcoal-color)] hover:bg-[color:var(--light-blue-charcoal-color)] rounded py-2 px-4 mt-3 sm:mt-0">
                     <ChevronUpIcon size={16} className="mr-2" />
                     <span className="text-sm">{post.upvotes}</span>
                   </button>
