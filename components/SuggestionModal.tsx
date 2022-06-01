7c2453d2974ab27aeb50b720224b02a0c2f42a91
ryuuto829 <ryuuto829@gmail.com>
2022-06-01 19:18:35 +0300

    refactor: update modals

--- components/SuggestionModal.tsx
+++ components/SuggestionModal.tsx
@@ -14,21 +14,30 @@ export default function SuggestionForm({}: SuggestionModalProps) {
   // console.log(errors)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="text"
-        placeholder="Title"
-        className="bg-transparent"
-        {...register('Title', { required: true, max: 150, min: 2, maxLength: 150 })}
-      />
-      <select {...register('Category', { required: true })} className="bg-transparent">
-        <option value="option1">option1</option>
-      </select>
-      <textarea
-        {...register('Content', { max: 1000, maxLength: 1000 })}
-        className="bg-transparent"
-      />
-      <input type="submit" />
-    </form>
+    <div>
+      <Dialog.Title className="text-xl font-bold text-white sm:text-2xl w-full pb-6">
+        Make a suggestion
+      </Dialog.Title>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col text-left">
+        <label htmlFor="title" className="text-sm text-gray-400">
+          Title
+        </label>
+        <input
+          id="title"
+          type="text"
+          placeholder="A short, descriptive title"
+          className="bg-transparent"
+          {...register('Title', { required: true, max: 150, min: 2, maxLength: 150 })}
+        />
+        <select {...register('Category', { required: true })} className="bg-transparent">
+          <option value="option1">option1</option>
+        </select>
+        <textarea
+          {...register('Content', { max: 1000, maxLength: 1000 })}
+          className="bg-transparent"
+        />
+        <input type="submit" />
+      </form>
+    </div>
   )
 }
