17d5a70d0d6194b991dea64663dcdbd60ef556fa
ryuuto829 <ryuuto829@gmail.com>
2022-06-04 16:19:51 +0300

    build: add next seo default config

---
+++ next-seo.config.js
@@ -0,0 +1,25 @@
+const title = 'Next Suggestions App – Let us know how we can improve'
+const description = 'Vote on existing ideas or suggest new ones.'
+
+const SEO = {
+  title,
+  description,
+  canonical: 'https://example.com',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://example.com',
+    title,
+    description,
+    images: [
+      {
+        url: 'https://example.com/og.png',
+        alt: title,
+        width: 1280,
+        height: 720,
+      },
+    ],
+  },
+}
+
+export default SEO
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
-import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+
+import '@styles/globals.css'
 import { AuthProvider } from '@lib/auth'
+import SEO from '../next-seo.config'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </AuthProvider>
   )
